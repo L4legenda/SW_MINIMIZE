e2e52777e28d0025b0c69816e785d68267a908f1
l4legenda <l4legenda@gmail.com>
2021-01-28 15:52:30 +0500

    model

--- SW_MINIMIZE.py
+++ SW_MINIMIZE.py
@@ -7,11 +7,12 @@ winlist = []
 def enum_callback(hwnd, results):
     winlist.append((hwnd, win32gui.GetWindowText(hwnd)))
 
-win32gui.EnumWindows(enum_callback, toplist)
-firefox = [(hwnd, title) for hwnd, title in winlist if 'firefox' in title.lower()]
-# just grab the first window that matches
-firefox = firefox[0]
-# use the window handle to set focus
-win32gui.SetForegroundWindow(firefox[0])
+def MINIMIZE():
+    win32gui.EnumWindows(enum_callback, toplist)
+    firefox = [(hwnd, title) for hwnd, title in winlist if 'firefox' in title.lower()]
+    # just grab the first window that matches
+    firefox = firefox[0]
+    # use the window handle to set focus
+    win32gui.SetForegroundWindow(firefox[0])
 
-win32gui.ShowWindow(firefox[0], win32con.SW_MINIMIZE)
+    win32gui.ShowWindow(firefox[0], win32con.SW_MINIMIZE)
